a0115fa610e03cf52d532edc048ac4ce28676082
wei <734099013@qq.com>
2018-01-19 15:39:09 +0800

    first commit

---
+++ src/com/example/collecttest/TestCollect.java
@@ -0,0 +1,13 @@
+package com.example.collecttest;
+
+
+public class TestCollect {
+
+    public static void main(String [] args){
+        System.out.println("Hello world！");
+        System.out.println("Hello  Buaa！");
+    }
+
+
+
+}
