7afb6ae99e5b90707ba99f8c14a551a80127d139
wei <734099013@qq.com>
2018-01-19 15:53:34 +0800

    ceconf commit

--- src/com/example/collecttest/TestCollect.java
+++ src/com/example/collecttest/TestCollect.java
@@ -1,11 +1,22 @@
 package com.example.collecttest;
 
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class TestCollect {
 
     public static void main(String [] args){
         System.out.println("Hello world！");
         System.out.println("Hello  Buaa！");
+
+        for(int i=0 ;i<10;i++){
+            System.out.println("This is number of"+i);
+        }
+
+        List<String> myList=new ArrayList<>();
+
+        //nihao zheshi zhushi 
     }
 
 
