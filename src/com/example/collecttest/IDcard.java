c5d064d7be4d40199f5faa1e4ccdce5e45eb3a16
wei <734099013@qq.com>
2018-01-19 22:48:04 +0800

    -a

---
+++ src/com/example/collecttest/IDcard.java
@@ -0,0 +1,14 @@
+package com.example.collecttest;
+
+public class IDcard {
+    int salary ;
+    int  name;
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+}
